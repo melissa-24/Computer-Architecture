c057dd49f7e0d2b82b5f39838975c80cc0c72ded
BeeDev Services <melissa@beedev-services.com>
2020-12-11 22:27:36 -0500

    forgot to push changes

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,40 +2,68 @@
 
 import sys
 
+LDI = 0b10000010
+PRN = 0b01000111
+HLT = 0b00000001
+ADD = 0b10100000
+MUL = 0b10100010
+PUSH = 0b01000101
+POP = 0b01000110 
+JMP = 0b01010100
+CALL = 0b01010000
+RET = 0b00010001
+ST = 0b10000100
+
+SP = 7
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0x00    
+        self.halted = False     
+        self.reg[SP] = 0xF4
+        self.end_of_stack = 0x00       
+        
 
-    def load(self):
+    def ram_read(self, MAR):
+        return self.ram[MAR]
+
+    def ram_write(self, MDR, MAR):
+        self.ram[MAR] = MDR
+
+    def load(self, filename):
         """Load a program into memory."""
 
-        address = 0
+        MAR = 0x00
 
-        # For now, we've just hardcoded a program:
+        try:
+            with open(filename) as program:
+                for line in program:
+                    comment_split = line.split("#")
+                    try:
+                        MDR = int(comment_split[0], 2)
+                        self.ram_write(MDR, MAR)
+                        MAR += 1
+                    except:
+                        continue
+        except FileNotFoundError:
+            print("File Not Found...")
+            sys.exit(1)
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        self.end_of_stack = MAR
 
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
-        if op == "ADD":
+        if op == ADD:
             self.reg[reg_a] += self.reg[reg_b]
+        elif op == MUL:
+            self.reg[reg_a] *= self.reg[reg_b]
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
@@ -62,4 +90,93 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+
+        while not self.halted:
+            IR = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+            
+            self.execute_instruction(IR, operand_a, operand_b)
+
+    def execute_instruction(self, IR, operand_a, operand_b):
+        number_of_operands = ((IR >> 6) & 0b11)
+
+        branchtable = {
+            LDI: self.handle_LDI,
+            PRN: self.handle_PRN,
+            HLT: self.handle_HLT,
+            ADD: self.handle_ALU,
+            MUL: self.handle_ALU,
+            PUSH: self.handle_PUSH,
+            POP: self.handle_POP,
+            JMP: self.handle_JUMP,
+            CALL: self.handle_CALL,
+            RET: self.handle_RET,
+            ST: self.handle_ST
+        }
+
+        if IR not in branchtable:
+            print(f"Unknown instruction {IR}")
+            self.halted = True
+        else:    
+            if number_of_operands == 2:     
+                branchtable[IR](operand_a, operand_b)
+            elif number_of_operands == 1:
+                branchtable[IR](operand_a)
+            else:
+                branchtable[IR]()
+
+
+        if IR != JMP and IR != CALL and IR != RET:
+            self.pc += number_of_operands + 1
+
+
+    # Start of Operation Handles
+    def handle_LDI(self, operand_a, operand_b):
+        self.reg[operand_a] = operand_b
+    
+    def handle_PRN(self, operand_a):
+        print(self.reg[operand_a])
+
+    def handle_HLT(self):
+        self.halted = True
+    
+    def handle_ALU(self, operand_a, operand_b):
+        IR = self.ram_read(self.pc)
+        self.alu(IR, operand_a, operand_b)
+
+    def handle_PUSH(self, operand_a):
+        if self.reg[SP] - 1 < self.end_of_stack:
+            print("STACK OVERFLOW! Exceeded maximum allocated space for the stack...")
+            sys.exit(1)
+        self.reg[SP] -= 1
+        value_in_register = self.reg[operand_a]
+        self.ram_write(value_in_register, self.reg[SP])
+
+    def handle_POP(self, operand_a):
+        if self.reg[SP] < 0xF4:
+            value_in_stack_pointer_register = self.ram_read(self.reg[SP])
+            self.reg[operand_a] = value_in_stack_pointer_register
+            self.reg[SP] += 1
+        else:
+            print("STACK UNDERFLOW! Stack is empty...")
+            sys.exit(1)
+    
+    def handle_JUMP(self, operand_a):
+        address_to_jump_to = self.reg[operand_a]
+        self.pc = address_to_jump_to
+
+    def handle_CALL(self, operand_a):
+        self.reg[SP] -= 1
+        self.ram_write(self.pc + 2, self.reg[SP])
+        self.pc = self.reg[operand_a]
+
+    def handle_RET(self):
+        self.pc = self.ram_read(self.reg[SP])
+        self.reg[SP] += 1
+    
+    def handle_ST(self, operand_a, operand_b):
+        self.reg[operand_a] = self.reg[operand_b]
+
+
+    # End of Operation Handles 
--- ls8/ls8.py
+++ ls8/ls8.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 """Main."""
 
 import sys
@@ -7,5 +5,9 @@ from cpu import *
 
 cpu = CPU()
 
-cpu.load()
+if len(sys.argv) < 2:
+    print("Incorrect number of arguments")
+    sys.exit(1)
+
+cpu.load(sys.argv[1])
 cpu.run()
